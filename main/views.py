e84ef2953ffcf7c15d021bfa72c42d03c6744cf7
theretari <theretarianingsih@gmail.com>
2024-09-09 23:11:17 +0700

    Memperbaiki html dan memperbaiki views

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'nama' : 'Theresia Tarianingsih',
+        'kelas' : 'PBP C',
         'name' : 'owala',
         'price': '800000',
         'description': 'FreeSip water bottle'
