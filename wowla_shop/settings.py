b1d826f491db89e8cb25bf7d29d0e0a0aae65190
theretari <theretarianingsih@gmail.com>
2024-09-15 18:31:03 +0700

    melakukan push ke pws berusaha karena tadi error

--- wowla_shop/settings.py
+++ wowla_shop/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://theresia-tarianingsih-wowlashopp.pbp.cs.ui.ac.id/", "https://theresia-tarianingsih-wowlashopp.pbp.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://theresia-tarianingsih-wowlashopp.pbp.cs.ui.ac.id", "https://pbp.cs.ui.ac.id/theresia.tarianingsih/wowlashopp"]
